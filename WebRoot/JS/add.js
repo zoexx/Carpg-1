3310c7b979534cc12ef751c9be2474e6d0f96996
zoexx <296852455@qq.com>
2014-12-03 10:08:20 +0800

    Signed-off-by: zoexx <296852455@qq.com>

--- WebRoot/JS/add.js
+++ WebRoot/JS/add.js
@@ -71,31 +71,31 @@ function checkSelect (objID) {
 }
 function doCheck () {
 	if (document.getElementById("type").value=="default") {
-		alert("请填写问题部位")；
+		alert("请填写问题部位");
 		return false;
 	}
 	if (document.getElementById("problem").value=="default") {
-		alert("请添加问题描述")；
+		alert("请添加问题描述");
 		return false;
 	}
 	if (document.getElementById("start_time").value=="") {
-		alert("请选择发生时间")；
+		alert("请选择发生时间");
 		//document.getElementById("start_time").style.border="1px solid #ABADB3";
 		return false;
 	}
 	var objss=document.getElementsByName("course_detile");
 	var toSub=document.getElementById("course");
 	for (var i = 0; i < objss.length; i++) {
-		if (objss.length[i].checked) {
+		if (objss[i].checked=="checked") {
 			toSub.value=toSub.value+"~"+objss.length[i].nextSibling.value;
 		}
 	}
 	if (document.getElementById("course_feel").value=="default") {
-		alert("请选择故事感受")；
+		alert("请选择故事感受");
 		return false;
 	}
 	if (document.getElementById("mark").value==null) {
-		alert("您的详细经过将更有助于维护您的权益，请填写事件的详细经过")；
+		alert("您的详细经过将更有助于维护您的权益，请填写事件的详细经过");
 	}
 	document.getElementById("form").action="";
 	document.getElementById("form").submit();
