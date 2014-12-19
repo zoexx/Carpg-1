785ceb3be8dd56c8919c472c26ebe8f9ffffc82a
yan1234 <1054974640@qq.com>
2014-12-19 14:26:36 +0800

    Signed-off-by: yan1234 <1054974640@qq.com>

--- src/com/carpg/util/Main.java
+++ src/com/carpg/util/Main.java
@@ -1,22 +1,27 @@
 package com.carpg.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.mail.MessagingException;
 
 public class Main {
+	static int id;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JavaMail mail = new JavaMail();
-		try {
-			mail.sendVerify("1054974640@qq.com", "yan", "12345678", "");
-		} catch (MessagingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		id = 4;
+		Main m = new Main();
+		m.setI();
+		System.out.println("id£º"+id);
 
+	}
+	public void setI(){
+		id = 3;
 	}
 
 }
