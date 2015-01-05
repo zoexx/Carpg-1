6ce5e302a90ffaaee92cec6eba3d1c569e28b0cb
zoexx <296852455@qq.com>
2015-01-05 21:17:32 +0800

    添加加载新闻列表 Signed-off-by: zoexx <296852455@qq.com>

--- WebRoot/JS/loadnews.js
+++ WebRoot/JS/loadnews.js
@@ -1,17 +1,7 @@
 
-var newsCount;//设置计数器
-//加载新闻列表
-//var newsJson=<%=新闻列表json%>;
-var jakeJson=[
-{"car":"沃尔沃XC60/S60L","seller":"浙江豪情汽车","time":"12-31","source":"国家质检总局","imgSrc":"../images/img/newsSmallPic.png","nTitle":"浙江豪情汽车召回部分国产沃尔沃XC60/S60L","content":"&nbsp;&nbsp;&nbsp;&nbsp;日前，浙江豪情汽车制造有限公司根据《缺陷汽车产品召回管理条例》的要求，向国家质检总局备案了召回计划，决定自2014年12月31日起，召回2014年11月12日至2014年11月19日期间生产的部分国产2015款沃尔沃XC60及S60L汽车，共计529辆。","praise":11},
-{"car":"沃尔沃XC60/S60L","seller":"浙江豪情汽车","time":"12-31","source":"国家质检总局","imgSrc":"../images/img/newsSmallPic.png","nTitle":"浙江豪情汽车召回部分国产沃尔沃XC60/S60L","content":"&nbsp;&nbsp;&nbsp;&nbsp;日前，浙江豪情汽车制造有限公司根据《缺陷汽车产品召回管理条例》的要求，向国家质检总局备案了召回计划，决定自2014年12月31日起，召回2014年11月12日至2014年11月19日期间生产的部分国产2015款沃尔沃XC60及S60L汽车，共计529辆。","praise":11},
-{"car":"沃尔沃XC60/S60L","seller":"浙江豪情汽车","time":"12-31","source":"国家质检总局","imgSrc":"../images/img/newsSmallPic.png","nTitle":"浙江豪情汽车召回部分国产沃尔沃XC60/S60L","content":"&nbsp;&nbsp;&nbsp;&nbsp;日前，浙江豪情汽车制造有限公司根据《缺陷汽车产品召回管理条例》的要求，向国家质检总局备案了召回计划，决定自2014年12月31日起，召回2014年11月12日至2014年11月19日期间生产的部分国产2015款沃尔沃XC60及S60L汽车，共计529辆。","praise":11},
-];
-var newsJson=jakeJson;
-//var nColumn=<%=汽车召回or缺陷调查%>
-setNewsColumn(nColumn);//设置新闻栏目名称
-function loadnews () {
-	var maxsize;//设置每页展示新闻条数
+
+function loadnews (maxsize) {
+	//maxsize设置每页展示新闻条数
 	for (var i = 0; i < maxsize; i++) {
 		showNews(i);
 		newsCount++;
@@ -25,7 +15,11 @@ function showNews (i) {
 	var ali=document.createElement("li");
 	ali.className="newsPreContent";
 	ali.value=newsCount;
-	ali.onclick="toDetailPage(this)";
+	ali.onclick=function (this){
+	var str=JSON.stringify(newsJson[obj.value]);
+	alert(str);
+	window.location.href=""+encodeURI(encodeURI('news_detail.jsp?param='+str));
+}
 	var ap=document.createElement("p");
 	var aspan=document.createElement("span");
 	aspan.className="newsCategory left";
@@ -52,14 +46,14 @@ function showNews (i) {
 	var bp=document.createElement("p");
 	bp.className="newsPreText";
 	var ahref=document.createElement("a");
-	ahref="#";
+	ahref.href="#";
 	var ahh=document.createElement("h2");
 	ahh.innerHTML=""+newsJson[i].nTitle;
 	var bhh=document.createElement("h5");
 	bhh.innerHTML=""+getPreviewText(newsJson[i].content);
+	ahref.appendChild(ahh);
+	ahref.appendChild(bhh);
 	bp.appendChild(ahref);
-	bp.appendChild(ahh);
-	bp.appendChild(bhh);
 	ali.appendChild(bp);
 	var espan=document.createElement("span");
 	espan.className="redLineNub right";
@@ -68,7 +62,7 @@ function showNews (i) {
 	document.getElementById("newsListContent").appendChild(ali);
 }
 function getPreviewText (str) {
-	var contentLength=101;//设置截取段落的长度
+	var contentLength=100;//设置截取段落的长度
 	//预览内容，截取文章第一段的101个字，并加上....省略号
 	//获得第一段，拆开第一个p，如果为空拆下一个
 	var a=str.split("</p>");
@@ -77,16 +71,12 @@ function getPreviewText (str) {
 		 var ainner=a[i].split(">");
 		 if (ainner[1]) {
 		 	atext=ainner[1];
-		 }else{
-		 	continue;
+		 	break;
 		 }
 	}
-	
-	var stext=ainner[1];
-	//字符串长度如果小于101，如果大于101，只截取到101
-	//加上省略号输出
-}
-function toDetailPage(obj){
-	//传newsJson[obj.value]过去下一个页面；
-	
+	if(atext.length>contentLength){
+		atext=atext.substring(0,99);
+	}
+	atext+="......";//加上省略号输出
+	return atext;
 }
--- WebRoot/JSP/news.jsp
+++ WebRoot/JSP/news.jsp
@@ -11,6 +11,7 @@
 		<link rel="stylesheet" href="../CSS/new_layout.css" />
 		<link rel="stylesheet" href="../CSS/color.css" />
 	    <script type="text/javascript" src="../JS/util.js" ></script>
+	    <script src="../JS/loadnews.js" type="text/javascript" charset="utf-8"></script>
 	</head>
 	<body>
 <jsp:include page="header.jsp" flush="true" ></jsp:include>
@@ -25,7 +26,19 @@
 					<li class="view_newsTitle">
 						<h2 class="newsGrey"><b id="nColumn">汽车召回</b></h2>
 					</li>
-			 <jsp:include page="/HTML/left_newslist.html"></jsp:include>
+					<script type="text/javascript">
+var newsCount=0;//设置计数器
+var jakeJson=[
+{"car":"沃尔沃XC60/S60L","seller":"浙江豪情汽车","time":"12-31","source":"国家质检总局","imgSrc":"../images/img/newsSmallPic.png","nTitle":"浙江豪情汽车召回部分国产沃尔沃XC60/S60L","content":"<p style=\"color=black\">&nbsp;&nbsp;&nbsp;&nbsp;日前，浙江豪情汽车制造有限公司根据《缺陷汽车产品召回管理条例》的要求，向国家质检总局备案了召回计划，决定自2014年12月31日起，召回2014年11月12日至2014年11月19日期间生产的部分国产2015款沃尔沃XC60及S60L汽车，共计529辆。</p>","praise":11},
+{"car":"沃尔沃XC60/S60L","seller":"浙江豪情汽车","time":"12-31","source":"国家质检总局","imgSrc":"../images/img/newsSmallPic.png","nTitle":"浙江豪情汽车召回部分国产沃尔沃XC60/S60L","content":"<p style=\"color=black\">&nbsp;&nbsp;&nbsp;&nbsp;日前，浙江豪情汽车制造有限公司根据《缺陷汽车产品召回管理条例》的要求，向国家质检总局备案了召回计划，决定自2014年12月31日起，召回2014年11月12日至2014年11月19日期间生产的部分国产2015款沃尔沃XC60及S60L汽车，共计529辆。</p>","praise":11},
+{"car":"沃尔沃XC60/S60L","seller":"浙江豪情汽车","time":"12-31","source":"国家质检总局","imgSrc":"../images/img/newsSmallPic.png","nTitle":"浙江豪情汽车召回部分国产沃尔沃XC60/S60L","content":"<p style=\"color=black\">&nbsp;&nbsp;&nbsp;&nbsp;日前，浙江豪情汽车制造有限公司根据《缺陷汽车产品召回管理条例》的要求，向国家质检总局备案了召回计划，决定自2014年12月31日起，召回2014年11月12日至2014年11月19日期间生产的部分国产2015款沃尔沃XC60及S60L汽车，共计529辆。</p>","praise":11},
+];
+var newsJson=jakeJson;
+var nColumn="汽车召回";
+setNewsColumn(nColumn);//设置新闻栏目名称	
+loadnews(3);//数字表示展示新闻的条数
+					</script>
+			 <!--<jsp:include page="/HTML/left_newslist.html"></jsp:include>-->
 			 	</ul>
 		    </div>
 		</div>
