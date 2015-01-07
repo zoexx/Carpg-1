e612db7e6cfe1aa8a7e7bac33a74f39ab1019cf5
yanling <1054974640@qq.com>
2015-01-07 19:12:20 +0800

    测试完成用户操作模块，发布新闻模块，抱怨模块以及调整统计信息后台处理
    Signed-off-by: yanling <1054974640@qq.com>

--- src/com/carpg/controller/StatisticAction.java
+++ src/com/carpg/controller/StatisticAction.java
@@ -22,13 +22,6 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class StatisticAction extends ActionSupport implements ServletRequestAware,ServletResponseAware{
 	
-	//创建静态字符串便于维护和修改
-	public static String BRAND_YEAR_COUNT = "brand_year_count"; //品牌按年份统计
-	public static String BRAND_COUNT_YEAR = "brand_count_year"; //某一年内各个品牌的状况统计
-	public static String BRAND_CARTYPE_COUNT = "brand_cartype_count"; //某一个品牌中各个车型的状况统计
-	public static String PROBLEM_YEAR_COUNT = "problem_year_count"; //问题按年份统计
-	public static String PROBLEM_COUNT_YEAR = "problem_count_year"; //某一年内各个问题的状况统计
-	public static String PROBLEM_CARTYPE_COUNT = "problem_cartype_count"; //某一个问题中各个车型的状况统计
 	
 	private HttpServletRequest request;
 	private HttpServletResponse response;
@@ -45,27 +38,24 @@ public class StatisticAction extends ActionSupport implements ServletRequestAwar
 	}
 	//根据参数类别获取排行榜
 	public String getRank() throws Exception{
-		//根据type的类别执行操作
-		if (type.equals(BRAND_YEAR_COUNT)){
-			//执行操作
-			StatisticDao staDao = new StatisticImpl();
-			LinkedHashMap<String, Integer> map = (LinkedHashMap<String, Integer>)staDao.getCountByYear_brand(param);
-			Set set = map.entrySet();
-			Iterator iterator = set.iterator();
-			JSONArray array = new JSONArray();
-			//遍历hashMap并载入到数据源(JSONArray)
-			while (iterator.hasNext()){
-				 Map.Entry element = (Map.Entry)iterator.next(); 
-		         String  key = (String)element.getKey(); 
-		         int  value = (Integer)element.getValue();
-		         JSONObject obj = new JSONObject();
-		         obj.put("name", key);
-		         obj.put("nub", value);
-		         array.add(obj);
-			}
-			msg = array.toString();
-			
+		//执行操作
+		StatisticDao staDao = new StatisticImpl();
+		//调用中间的调度层control函数
+		LinkedHashMap<String, Integer> map = (LinkedHashMap<String, Integer>)staDao.control(type, param);
+		Set set = map.entrySet();
+		Iterator iterator = set.iterator();
+		JSONArray array = new JSONArray();
+		//遍历hashMap并载入到数据源(JSONArray)
+		while (iterator.hasNext()){
+			 Map.Entry element = (Map.Entry)iterator.next(); 
+	         String  key = (String)element.getKey(); 
+	         int  value = (Integer)element.getValue();
+	         JSONObject obj = new JSONObject();
+	         obj.put("name", key);
+	         obj.put("nub", value);
+	         array.add(obj);
 		}
+		msg = array.toString();
 		return "rank";
 	}
 	
